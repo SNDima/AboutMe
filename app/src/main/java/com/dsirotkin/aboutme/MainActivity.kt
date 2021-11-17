343ab2119098ca13f7cf57632c062490995b3da9
dsirotkin <d.sirotkin.n@gmail.com>
2021-11-17 23:00:42 +0300

    A button and a listener was added.

--- app/src/main/java/com/dsirotkin/aboutme/MainActivity.kt
+++ app/src/main/java/com/dsirotkin/aboutme/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.dsirotkin.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.dsirotkin.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        val editText = binding.nicknameEdit
+        val nicknameTextView = binding.nicknameText
+
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        // Hide the keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
